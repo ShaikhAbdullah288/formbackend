b83414ab27192b63a238c8addf74de45402914eb
abdullah <shaikhabdullah9890@gmail.com>
2026-03-30 20:11:06 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const multer = require("multer");
+
+const app = express();
+app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/jobDB")
+  .then(() => console.log("DB Connected"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "uploads"),
+  filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname)
+});
+
+const upload = multer({ storage });
+
+const Job = mongoose.model("Job", {
+  name: String,
+  email: String,
+  phone: String,
+  role: String,
+  coverLetter: String, 
+  resume: String
+});
+
+app.post("/api/jobs", upload.single("resume"), async (req, res) => {
+  try {
+    const job = new Job({
+      ...req.body,
+   resume: req.file ? req.file.filename : null
+    });
+
+    await job.save();
+    res.json({ message: "Application submitted!" });
+  } catch (err) {
+    console.log("Error:", err);
+    res.status(500).json({ message: "Error" , error: err.message});
+  }
+});
+
+app.listen(5000, () => console.log("Server running"));
