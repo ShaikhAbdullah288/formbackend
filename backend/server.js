61179bad0ce4a8a47076fa4df0e7ec174e3a1bdd
ShaikhAbdullah288 <79159665+ShaikhAbdullah288@users.noreply.github.com>
2026-03-31 21:27:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,30 +4,27 @@ const cors = require("cors");
 const multer = require("multer");
 
 const app = express();
+
 app.use(cors({
-  origin: "https://jobform-henna.vercel.app"
+  origin: "https://jobform-henna.vercel.app",
   methods: ["GET", "POST"],
   credentials: true
-
 }));
 
+app.use(express.json());
+
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("DB Connected"));
+  .then(() => console.log("DB Connected"))
+  .catch(err => console.log(err));
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "uploads"),
-  filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname)
-});
-
-
-const upload = multer({ storage });
+const upload = multer({ storage: multer.memoryStorage() });
 
 const Job = mongoose.model("Job", {
   name: String,
   email: String,
   phone: String,
   role: String,
-  coverLetter: String, 
+  coverLetter: String,
   resume: String
 });
 
@@ -35,17 +32,16 @@ app.post("/api/jobs", upload.single("resume"), async (req, res) => {
   try {
     const job = new Job({
       ...req.body,
-   resume: req.file ? req.file.filename : null
+      resume: req.file ? req.file.buffer.toString("base64") : null
     });
 
     await job.save();
+
     res.json({ message: "Application submitted!" });
   } catch (err) {
     console.log("Error:", err);
-    res.status(500).json({ message: "Error" , error: err.message});
+    res.status(500).json({ message: "Error", error: err.message });
   }
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log("Server running"));
+module.exports = app;
